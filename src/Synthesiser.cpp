9d76bc90dc98f91d3c24afd7c231993f0b14a747
David Zhao <dzha3983@uni.sydney.edu.au>
2020-12-16 13:41:12 +1100

    fix aggregates

--- src/Synthesiser.cpp
+++ src/Synthesiser.cpp
@@ -1497,9 +1497,9 @@ void Synthesiser::emitCode(std::ostream& out, const RamStatement& stmt) {
 
             if (aggregate.getFunction() == souffle::MIN || aggregate.getFunction() == souffle::MAX) {
                 // check whether there exists a min/max first before next loop
-                out << "if(res" << identifier << " != " << init << "){\n";
+                // out << "if(res" << identifier << " != " << init << "){\n";
                 visitTupleOperation(aggregate, out);
-                out << "}\n";
+                // out << "}\n";
             } else {
                 visitTupleOperation(aggregate, out);
             }
@@ -1591,9 +1591,9 @@ void Synthesiser::emitCode(std::ostream& out, const RamStatement& stmt) {
 
             if (aggregate.getFunction() == souffle::MIN || aggregate.getFunction() == souffle::MAX) {
                 // check whether there exists a min/max first before next loop
-                out << "if(res" << identifier << " != " << init << "){\n";
+                // out << "if(res" << identifier << " != " << init << "){\n";
                 visitTupleOperation(aggregate, out);
-                out << "}\n";
+                // out << "}\n";
             } else {
                 visitTupleOperation(aggregate, out);
             }
